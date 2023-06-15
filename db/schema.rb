c6bdfccd09e8e160230f3084bd8e4fecc0b2f246
theGeekkit <sonofgondolin16@gmail.com>
2023-06-15 18:49:19 -0500

    6-15-23

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_05_202226) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -19,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_202226) do
 
   create_table "tickets", force: :cascade do |t|
     t.string "number"
-    t.integer "customer_id", null: false
+    t.bigint "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_tickets_on_customer_id"
